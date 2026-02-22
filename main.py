df540325000afcc7c02c6e56364a197a04387148
Vaibhav S Gowda <162559841+Vaibhav-S-Gowda@users.noreply.github.com>
2026-02-22 21:25:19 +0530

    explored http requests

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 from fastapi import FastAPI
 from fastapi.params import Body
+from pydantic import BaseModel
 
 app = FastAPI()
 
+class Post(BaseModel):
+    title: str
+    content: str
+    
 @app.get("/")
 async def root():
     return {"message": "Hello World!"}
@@ -11,7 +16,13 @@ async def root():
 def get_posts():
     return {"data": "This is your post"}
 
-@app.post("/createposts")
+'''@app.post("/createposts")
 def create_posts(payLoad: dict = Body(...)):
     print(payLoad)
     return {"new_post": [f"title is {payLoad['title']}, content = {payLoad['content']}"]}
+# title str, content str'''
+
+@app.post("/createposts")
+def create_posts(new_post: Post):
+    print(new_post.title)
+    return {"data": "new post"}
