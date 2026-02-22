2e321b0813f29360f1ed786f37bd8a97b3146551
Vaibhav S Gowda <162559841+Vaibhav-S-Gowda@users.noreply.github.com>
2026-02-22 21:51:22 +0530

    changes are made

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 from fastapi import FastAPI
 from fastapi.params import Body
 from pydantic import BaseModel
+from typing import Optional
 
 app = FastAPI()
 
 class Post(BaseModel):
     title: str
     content: str
+    published: bool = True
+    rating: Optional[int] = None
     
 @app.get("/")
 async def root():
@@ -24,5 +27,6 @@ def create_posts(payLoad: dict = Body(...)):
 
 @app.post("/createposts")
 def create_posts(new_post: Post):
-    print(new_post.title)
-    return {"data": "new post"}
+    print(new_post)
+    print(new_post.dict())
+    return {"data": new_post}
